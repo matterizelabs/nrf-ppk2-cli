bf770675c5d11ce83cd071a9319cc399b0fc026c
abu-matterize <abu@matterize.io>
2026-07-24 19:20:42 +0530

    chore: add nix flake dev environment with fenix stable rust

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "PPK2 CLI development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, fenix }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          toolchain = fenix.packages.${system}.stable;
+        in {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              toolchain.toolchain
+              pkgs.pkg-config
+            ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+              pkgs.libudev-zero
+            ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.darwin.apple_sdk.frameworks.IOKit
+              pkgs.darwin.apple_sdk.frameworks.Foundation
+            ];
+            shellHook = ''
+              echo "PPK2 dev shell | Rust $(rustc --version)"
+            '';
+          };
+        }
+      );
+    };
+}
